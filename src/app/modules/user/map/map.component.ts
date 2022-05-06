8717efa7cbeb2c2d11e760c56341195bd5cc6b1c
Anton Skrypii <thetonytekk@gmail.com>
2022-05-06 12:02:08 +0300

    Minor changes

--- src/app/modules/user/map/map.component.ts
+++ src/app/modules/user/map/map.component.ts
@@ -1,24 +1,25 @@
-const SVG_Marker = {
-    path: "M10.453 14.016l6.563-6.609-1.406-1.406-5.156 5.203-2.063-2.109-1.406 1.406zM12 2.016q2.906 0 4.945 2.039t2.039 4.945q0 1.453-0.727 3.328t-1.758 3.516-2.039 3.070-1.711 2.273l-0.75 0.797q-0.281-0.328-0.75-0.867t-1.688-2.156-2.133-3.141-1.664-3.445-0.75-3.375q0-2.906 2.039-4.945t4.945-2.039z",
-    fillColor: "blue",
-    fillOpacity: 1,
-    strokeWeight: 0,
-    rotation: 0,
-    scale: 2,
-    anchor: new google.maps.Point(15, 30),
-};
-
-const CITY_CENTER = {
-    lat: 50.57743527945256,
-    lng: 30.24427711473898,
-};
-
 // Angular
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { MapInfoWindow, MapMarker, GoogleMap } from '@angular/google-maps';
 
 // App
 import { ObjectService } from '../../../services/object.service';
+
+// TODO remove to service
+const CITY_CENTER = {
+    lat: 50.57743527945256,
+    lng: 30.24427711473898,
+};
+
+//TODO remove to options
+const MAP_OPTIONS = {
+    mapTypeId: 'roadmap',
+    zoomControl: false,
+    scrollwheel: false,
+    disableDoubleClickZoom: true,
+    maxZoom: 150,
+    minZoom: 1,
+};
 
 @Component({
     selector: 'app-map',
@@ -33,19 +34,12 @@ export class MapComponent implements OnInit {
         public objectService: ObjectService,
     ) { }
 
-    // TODO Add type
+    // TODO Add call to backend
     public markers = this.objectService.getObjects();
 
     public zoom = 14;
     public center = CITY_CENTER;
-    public options = {
-        mapTypeId: 'roadmap',
-        zoomControl: false,
-        scrollwheel: false,
-        disableDoubleClickZoom: true,
-        maxZoom: 150,
-        minZoom: 1,
-    }
+    public options = MAP_OPTIONS;
 
     // TODO Add type
     public object = {
@@ -66,7 +60,8 @@ export class MapComponent implements OnInit {
     }
 
     public openInfo(markerElem: any, data: any) {
-        // console.log(data);
+        console.log('Info: ', data);
+
         this.object.title = data.title;
         this.object.description = data.description;
         this.object.image = data.image;
@@ -75,7 +70,7 @@ export class MapComponent implements OnInit {
         this.infoWindow.open(markerElem)
 
         //console.log(markerElem);
-        markerElem.marker.icon.url = './assets/check.svg';
+        //markerElem.marker.icon.url = './assets/check.svg';
     }
 
     public zoomIn() {
